cf63bcb60c9f45d269b8174f548eb6353654c3df
Eric Ekas <eric.ekas@gmail.com>
2022-08-04 11:17:28 -0400

    add session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :banks
     has_many :goals
     has_many :commits , through: :goals
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
